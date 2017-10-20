2f7ccd55087bc7cadf24c5b1ac0d35ca46680211
Steve Witman <stevewitman@gmail.com>
2017-10-19 19:28:37 -0600

    Install bootstrap and firebase, generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
